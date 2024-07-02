ddb34492bacb159ffbf659e90fc8eae3a39c1c37
Maikel Hofman <m.hofman@chronos-technology.nl>
2024-07-02 22:20:35 +0200

    Send correct info back to the client

--- task_1/main.go
+++ task_1/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
-	"io"
+	"math"
 	"net"
+	"net/textproto"
 )
 
 func main() {
 	fmt.Println("Problem 1 Protohacker")
-	fmt.Println("Starting server on port 8080")
+	fmt.Println("Starting server on port 15000")
 
-	listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":15000")
 	if err != nil {
 		fmt.Println("Error starting TCP server", err.Error())
 	}
@@ -32,14 +34,75 @@ func handleConnection(connection net.Conn) {
 	defer connection.Close()
 
 	c := bufio.NewReader(connection)
+	textReader := textproto.NewReader(c)
 	// read the full message, or return an error
-	buffer, err := io.ReadAll(c)
+	for {
+		line, err := textReader.ReadLine()
+		if err != nil {
+			fmt.Println("Error reading body: ", err.Error())
+			return
+		}
+
+		fmt.Println("Received message: ", line)
+		var request Request
+		err = json.Unmarshal([]byte(line), &request)
+		if err != nil || request.Number == nil || request.Method != "isPrime" {
+			response := Response{}
+			sendResponse(connection, response)
+			return
+		}
+
+		isPrime := IsPrime(*request.Number)
+		response := Response{
+			Method: "isPrime",
+		}
+		if isPrime {
+			fmt.Println("Number is prime")
+			response.Prime = true
+		}
+
+		sendResponse(connection, response)
+	}
+}
+
+func sendResponse(connection net.Conn, response Response) {
+	bytes, err := json.Marshal(response)
 	if err != nil {
-		fmt.Println("Error reading body")
+		fmt.Println("Error marshalling body", err.Error())
 		return
 	}
 
-	fmt.Printf("received %x\n", buffer)
+	// Append newline character
+	bytes = append(bytes, 10)
+	written, err := connection.Write(bytes)
+	if err != nil {
+		fmt.Println("Error responding to client", err.Error())
+		return
+	}
+
+	fmt.Println("Written ", written)
+}
+
+func IsPrime(number int64) bool {
+	if number <= 1 {
+		return false
+	}
+	if number == 2 {
+		return true
+	}
+	if number%2 == 0 {
+		return false
+	}
+
+	boundary := int(math.Floor(math.Sqrt(float64(number))))
+
+	for i := 3; i <= boundary; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 type Response struct {
@@ -49,5 +112,5 @@ type Response struct {
 
 type Request struct {
 	Method string `json:"method"`
-	Number int    `json:"number"`
+	Number *int64 `json:"number"`
 }
